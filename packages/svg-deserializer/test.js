dc8cf1911269fa9a1ddc332a2f185834c42504dc
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-09-26 11:46:08 +0200

    test(): updated imports to match new api for parsers * renamed two basic test functions for more clarity * also changed inputs for cag output tests

--- packages/svg-deserializer/test.js
+++ packages/svg-deserializer/test.js
@@ -1,7 +1,8 @@
 const test = require('tape')
 const {CSG, CAG} = require('@jscad/csg')
+const deserializer = require('./index.js')
 
-test('simple test svg to jscad code', function (t) {
+test('translate svg to jscad code', function (t) {
   t.plan(1)
 
   const sourceSvg = `
@@ -22,29 +23,24 @@ test('simple test svg to jscad code', function (t) {
   return cag0;
 }
 `
-  const observed = require('./index-jscad.js').deserialize(sourceSvg, undefined, {addMetaData: false})
+  const observed = deserializer.translate(sourceSvg, undefined, {addMetaData: false})
   t.equal(observed, expected)
 })
 
-/*
-test('simple test svg to csg', function (t) {
+
+test('deserialize svg to cag/csg objects', function (t) {
   t.plan(1)
 
-  const sourceSvg = `<?xml version="1.0"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
-  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+  const sourceSvg = `
 <svg xmlns="http://www.w3.org/2000/svg"
  width="467" height="462">
-  <rect x="80" y="60" width="250" height="250" rx="20"
-      style="fill:#ff0000; stroke:#000000;stroke-width:2px;" />
-  <rect x="140" y="120" width="250" height="250" rx="40"
-      style="fill:#0000ff; stroke:#000000; stroke-width:2px;
-      fill-opacity:0.7;" />
+  <rect x="80" y="60" width="250" height="250" rx="20"/>
+  <rect x="140" y="120" width="250" height="250" rx="40"/>
 </svg>`
 
   // const sourceSvg = fs.readFileSync('PATH/TO/file.svg')
 
   const expected = ``
-  const observed = svgDeSerializer(sourceSvg, {})
+  const observed = deserializer.deserialize(sourceSvg, undefined, {addMetaData: false})
   t.equal(observed, expected)
-})*/
+})
