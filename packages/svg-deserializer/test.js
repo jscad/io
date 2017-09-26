17641ed35a5b64cd6cefd436556c546799740e45
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-09-26 09:59:29 +0200

    test(svg-deserializer): updated tests to have basic verification of jscad output

--- packages/svg-deserializer/test.js
+++ packages/svg-deserializer/test.js
@@ -1,7 +1,33 @@
 const test = require('tape')
 const {CSG, CAG} = require('@jscad/csg')
-const svgDeSerializer = require('./index-csg.js')
+const svgDeSerializer = require('./index-csg.js').deserialize
 
+test('simple test svg to jscad code', function (t) {
+  t.plan(1)
+
+  const sourceSvg = `
+<svg xmlns="http://www.w3.org/2000/svg"
+ width="467" height="462">
+  <rect x="80" y="60" width="250" height="250" rx="20"/>
+  <rect x="140" y="120" width="250" height="250" rx="40"/>
+</svg>`
+
+  // const sourceSvg = fs.readFileSync('PATH/TO/file.svg')
+
+  const expected = `function main(params) {
+  var cag0 = new CAG();
+  var cag00 = CAG.roundedRectangle({center: [57.8556,-52.2111], radius: [35.277775,35.277775], roundradius: 5.644443999999999});
+  cag0 = cag0.union(cag00);
+  var cag01 = CAG.roundedRectangle({center: [74.7889,-69.1444], radius: [35.277775,35.277775], roundradius: 11.288887999999998});
+  cag0 = cag0.union(cag01);
+  return cag0;
+}
+`
+  const observed = svgDeSerializer(sourceSvg, undefined, {addMetaData: false})
+  t.equal(observed, expected)
+})
+
+/*
 test('simple test svg to csg', function (t) {
   t.plan(1)
 
@@ -17,10 +43,9 @@ test('simple test svg to csg', function (t) {
       fill-opacity:0.7;" />
 </svg>`
 
+  // const sourceSvg = fs.readFileSync('PATH/TO/file.svg')
 
-  //const sourceSvg = fs.readFileSync('PATH/TO/file.svg')
-
-  const expected = 42
+  const expected = ``
   const observed = svgDeSerializer(sourceSvg, {})
   t.equal(observed, expected)
-})
+})*/
