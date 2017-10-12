14937efdb11246a8baa7699b2a11e180ff68a503
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-10-12 23:31:57 +0200

    test(stl-deserializer): started fleshing out tests: * added a first one for ascii stl => jscad * added basics for same to csg & binary to jscad

--- packages/stl-deserializer/test.js
+++ packages/stl-deserializer/test.js
@@ -1,20 +1,143 @@
 const fs = require('fs')
+const path = require('path')
 const test = require('ava')
 const deserializer = require('./index.js')
 
-test('translate ascii stl to jscad code', function (t) {
-  const inputStlPath = './foo.stl'
-  const inputStl = fs.readFileSync(inputStlPath)
+const filesPath = path.resolve('../../node_modules/@jscad/sample-files') // require.resolve('@jscad/sample-files')
 
-  const expected = `function main(params) {
-  var cag0 = new CAG();
-  var cag00 = CAG.roundedRectangle({center: [57.8556,-52.2111], radius: [35.277775,35.277775], roundradius: 5.644443999999999});
-  cag0 = cag0.union(cag00);
-  var cag01 = CAG.roundedRectangle({center: [74.7889,-69.1444], radius: [35.277775,35.277775], roundradius: 11.288887999999998});
-  cag0 = cag0.union(cag01);
-  return cag0;
-}
+test('translate simple ascii stl to jscad code', function (t) {
+  const inputStlPath = path.resolve(filesPath, 'stl/testcube_ascii.stl')
+  const inputStl = fs.readFileSync(inputStlPath, 'utf8')
+
+  const expected = `function main() { return union(
+// objects: 1
+// object #1: triangles: 12
+polyhedron({ points: [
+	[1,0,0],
+	[1,1,0],
+	[0,0,0],
+	[1,1,0],
+	[0,1,0],
+	[0,0,0],
+	[0,1,0],
+	[0,1,1],
+	[0,0,0],
+	[0,1,1],
+	[0,0,1],
+	[0,0,0],
+	[1,1,0],
+	[1,1,1],
+	[0,1,0],
+	[1,1,1],
+	[0,1,1],
+	[0,1,0],
+	[1,1,1],
+	[1,1,0],
+	[1,0,0],
+	[1,0,1],
+	[1,1,1],
+	[1,0,0],
+	[1,0,1],
+	[1,0,0],
+	[0,0,0],
+	[0,0,1],
+	[1,0,1],
+	[0,0,0],
+	[1,1,1],
+	[1,0,1],
+	[0,0,1],
+	[0,1,1],
+	[1,1,1],
+	[0,0,1]],
+	polygons: [
+	[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[9,10,11],
+	[12,13,14],
+	[15,16,17],
+	[18,19,20],
+	[21,22,23],
+	[24,25,26],
+	[27,28,29],
+	[30,31,32],
+	[33,34,35]] })
+); }
 `
+
   const observed = deserializer.deserialize(inputStl, undefined, {output: 'jscad', addMetaData: false})
   t.deepEqual(observed, expected)
 })
+
+test('translate simple binary stl to jscad code', function (t) {
+  const inputStlPath = path.resolve(filesPath, 'stl/testcube_10mm.stl')
+  const inputStl = fs.readFileSync(inputStlPath)
+
+  const expected = `function main() { return union(
+// objects: 1
+// object #1: triangles: 12
+polyhedron({ points: [
+	[1,0,0],
+	[1,1,0],
+	[0,0,0],
+	[1,1,0],
+	[0,1,0],
+	[0,0,0],
+	[0,1,0],
+	[0,1,1],
+	[0,0,0],
+	[0,1,1],
+	[0,0,1],
+	[0,0,0],
+	[1,1,0],
+	[1,1,1],
+	[0,1,0],
+	[1,1,1],
+	[0,1,1],
+	[0,1,0],
+	[1,1,1],
+	[1,1,0],
+	[1,0,0],
+	[1,0,1],
+	[1,1,1],
+	[1,0,0],
+	[1,0,1],
+	[1,0,0],
+	[0,0,0],
+	[0,0,1],
+	[1,0,1],
+	[0,0,0],
+	[1,1,1],
+	[1,0,1],
+	[0,0,1],
+	[0,1,1],
+	[1,1,1],
+	[0,0,1]],
+	polygons: [
+	[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[9,10,11],
+	[12,13,14],
+	[15,16,17],
+	[18,19,20],
+	[21,22,23],
+	[24,25,26],
+	[27,28,29],
+	[30,31,32],
+	[33,34,35]] })
+); }
+`
+
+  const observed = deserializer.deserialize(inputStl, undefined, {output: 'jscad', addMetaData: false})
+  t.deepEqual(observed, expected)
+})
+
+
+/*test('deserialize simple ascii stl to cag/csg objects', function (t) {
+  const inputStlPath = path.resolve(filesPath, 'stl/testcube_ascii.stl')
+  const inputStl = fs.readFileSync(inputStlPath, 'utf8')
+
+  const observed = deserializer.deserialize(inputStl, undefined, {output: 'csg', addMetaData: false})
+  t.deepEqual(observed.polygons.length, 42)
+})*/
