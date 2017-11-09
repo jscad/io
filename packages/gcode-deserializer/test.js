fa80c6a84f772aaa0d56fb0f206a41bcc54d5cff
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-11-09 13:30:15 +0100

    chore(gcode-deserializer): clearly marked gcode-deserializer as not implemented

--- packages/gcode-deserializer/test.js
+++ packages/gcode-deserializer/test.js
@@ -6,18 +6,16 @@ const deserializer = require('./index.js')
 const filesPath = path.resolve('../../node_modules/@jscad/sample-files') // require.resolve('@jscad/sample-files')
 const polygonsFromCsg = csg => csg.polygons.map(x => x.vertices.map(vert => ([vert.pos.x, vert.pos.y, vert.pos.z])))
 
-test('translate simple gcode file to jscad code', function (t) {
+test.failing('translate simple gcode file to jscad code (NOT IMPLEMENTED!!!)', function (t) {
   const inputPath = path.resolve(filesPath, 'gcode/cube_10mm.gcode')
   const inputFile = fs.readFileSync(inputPath, 'utf8')
-  console.log('inputFile', inputFile)
-
   const expected = ``
 
   const observed = deserializer.deserialize(inputFile, undefined, {output: 'jscad', addMetaData: false})
   t.deepEqual(observed, expected)
 })
 
-test('deserialize simple gcode to cag/csg objects', function (t) {
+test.failing('deserialize simple gcode to cag/csg objects (NOT IMPLEMENTED!!!)', function (t) {
   const inputPath = path.resolve(filesPath, 'gcode/cube_10mm.gcode')
   const inputFile = fs.readFileSync(inputPath, 'utf8')
 
