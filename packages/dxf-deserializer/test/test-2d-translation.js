4a46453880eadd1308df2400299130c0ff76d846
Z3 Development <z3-dev@gfnews.net>
2017-12-09 13:18:02 +0900

    Added simple test for ELLIPSE

--- packages/dxf-deserializer/test/test-2d-translation.js
+++ packages/dxf-deserializer/test/test-2d-translation.js
@@ -17,7 +17,7 @@ test('ASCII DXF 2D Entities translated to JSCAD Scripts', t => {
   let ss1 = src1.split("\n")
   t.is(ss1.length,3)
 
-// DXF CIRCLE, translates to script with CAG.circle()
+// DXF CIRCLE, translates to script with CAG.circle
   let dxf2 = `0
 SECTION
 2
@@ -40,7 +40,7 @@ ENDSEC`
   let ss2 = src2.split("\n")
   t.is(ss2.length,7)
 
-// DXF LINE, translates to script with 'new CSG.Line3D'
+// DXF LINE, translates to script with CSG.Line2D.fromPoints
   let dxf3 = `0
 SECTION
 2
@@ -100,7 +100,7 @@ ENDSEC`
   let ss4 = src4.split("\n")
   t.is(ss4.length,7)
 
-// DXF LWPOLYLINE without bulges, translates to script with CSG.Path2D, appendPoint(), and CAG.fromPoints()
+// DXF LWPOLYLINE without bulges, translates to script with CSG.Path2D, appendPoint, and CAG.fromPoints
   let dxf5 = `0
 SECTION
 2
@@ -135,7 +135,7 @@ ENDSEC`
   let ss5 = src5.split("\n")
   t.is(ss5.length,10)
 
-// DXF LWPOLYLINE with bulges, translates to script with CSG.Path2D and CAG.fromPoints()
+// DXF LWPOLYLINE with bulges, translates to script with CSG.Path2D and appendArc, and CAG.fromPoints
   let dxf6 = `0
 SECTION
 2
@@ -177,6 +177,45 @@ ENDSEC`
   let src6 = deserialize(dxf6,'dxf6-test',{output: 'jscad'})
   let ss6 = src6.split("\n")
   t.is(ss6.length,10)
+
+// DXF ELLIPSE, translates to script with CAG.ellipse
+  let dxf7 = `0
+SECTION
+2
+ENTITIES
+  0
+ELLIPSE
+  8
+0
+ 10
+7.5
+ 20
+17.5
+ 30
+0.0
+ 11
+-2.5
+ 21
+0.0
+ 31
+0.0
+210
+0.0
+220
+0.0
+230
+1.0
+ 40
+0.4
+ 41
+0.0
+ 42
+6.283185307179586
+0
+ENDSEC`
+  let src7 = deserialize(dxf7,'dxf7-test',{output: 'jscad'})
+  let ss7 = src7.split("\n")
+  t.is(ss7.length,7)
 
 })
 
