718c1c13f71edffb67b948851fdfd772dc8117bb
Z3 Development <z3-dev@gfnews.net>
2018-05-06 17:30:38 +0900

    Changed to use V2 API, and support multiple CSG objects

--- packages/stl-serializer/index.js
+++ packages/stl-serializer/index.js
@@ -1,17 +1,44 @@
-const binarySerializer = require('./CSGToStlb').serialize
-const asciiSerializer = require('./CSGToStla').serialize
-const { ensureManifoldness } = require('@jscad/io-utils')
+/*
+JSCAD Object to STL Format Serialization
+
+## License
+
+Copyright (c) 2018 JSCAD Organization https://github.com/jscad
+
+All code released under MIT license
+
+Notes:
+1) CAG conversion to:
+     none
+2) CSG conversion to:
+     STL mesh
+3) Path2D conversion to:
+     none
+*/
+
+const {serializeBinary} = require('./CSGToStlb')
+const {serializeText} = require('./CSGToStla')
+
+const {ensureManifoldness} = require('@jscad/io-utils')
+const {isCSG} = require('@jscad/csg')
 
 const mimeType = 'application/sla'
 
-function serialize (data, options) {
+const serialize = (options, ...objects) => {
   const defaults = {
-    binary: true
+    binary: true,
+    statusCallback: null
   }
   options = Object.assign({}, defaults, options)
 
-  data = ensureManifoldness(data)
-  return options.binary ? binarySerializer(data, options) : asciiSerializer(data, options)
+  // only use valid CSG objects
+  let csgs = []
+  objects.forEach(function (object, i) {
+    if (isCSG(object) & object.polygons.length > 0) {
+      csgs.push(ensureManifoldness(object))
+    }
+  })
+  return options.binary ? serializeBinary(csgs, options) : serializeText(csgs, options)
 }
 
 module.exports = {
